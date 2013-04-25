870c17e60dcacc158c83dd17885eedb285894a63
Keenan Iban <arcknine@skolpad.org>
2013-04-25 15:53:38 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcknine::Application.config.secret_token = 'f57b0719a2e524401a8b6c8b7e1349f573503dc4289c8a5472ac4528bf0e96b5b4b3065953e1d15212401aedbda596631fa19d7a6df21391388c4ae33953adb0'
